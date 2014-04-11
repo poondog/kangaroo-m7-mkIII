601eadb6c6c20141c73403f590a38a03a2e6f3f5
poondog <markj338@gmail.com>
2014-04-11 23:17:37 +1000

    BUILD: delete repeated build config and un needed variables
    Signed-off-by: poondog <markj338@gmail.com>

--- build.sh
+++ build.sh
@@ -2,19 +2,6 @@
 
 # Kangaroo Kernel build script thanks to CyanogenMod, anarkia1976
 
-DEFCONFIG_FILE=m7_defconfig
-
-if [ -z "$DEFCONFIG_FILE" ]; then
-	echo "Need defconfig file(j1v-perf_defconfig)!"
-	exit -1
-fi
-
-if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
-	echo "No such file : arch/arm/configs/$DEFCONFIG_FILE"
-	exit -1
-fi
-
-
 # Kernel Version
 BASE_VER=".KANGAROO"
 VER=".M7.v022"
@@ -26,11 +13,10 @@ export LOCALVERSION=""`echo $VER`
 # toolchain
 export CROSS_COMPILE=${HOME}/repos/arm-cortex_a15-linux-gnueabihf-linaro_4.8.3-2014.03/bin/arm-cortex_a15-linux-gnueabihf-
 
-
 # make .config
+make m7_defconfig
 env KCONFIG_NOTIMESTAMP=true \
 make ARCH=arm export SUBARCH=arm CROSS_COMPILE=arm-eabi-
-make m7_defconfig
 
 # build the kernel
 
