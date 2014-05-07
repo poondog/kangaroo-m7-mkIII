53362805a2f7a4abac172b5377d969f6ca937b56
poondog <markj338@gmail.com>
2014-05-08 07:01:18 +1000

    ARCH: KRAIT: lower voltage min limit to 600mV It will break someones device. But some can take this voltage, so lets see what happens. #yolo
    Signed-off-by: poondog <markj338@gmail.com>

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -928,7 +928,7 @@ static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
 
 #ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
 #define CPU_VDD_MAX		1300
-#define CPU_VDD_MIN		700
+#define CPU_VDD_MIN		600
 
 static unsigned int cnt;
 
