83c7ef9c8c0eaaff05b52c94413c2274d79f42c6
poondog <markj338@gmail.com>
2014-05-21 23:12:14 +1000

    Revert "SCHED: fixup for m7"
    This reverts commit 847eaf194c4f7be9cf325f038185093f43a4dddf.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -774,6 +774,7 @@ static inline unsigned int do_avg_nr_running(struct rq *rq)
 
 static inline void inc_nr_running(struct rq *rq)
 {
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 	write_seqcount_begin(&rq->ave_seqcnt);
 	rq->ave_nr_running = do_avg_nr_running(rq);
 	rq->nr_last_stamp = rq->clock_task;
@@ -783,6 +784,8 @@ static inline void inc_nr_running(struct rq *rq)
 
 static inline void dec_nr_running(struct rq *rq)
 {
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
+	write_seqcount_begin(&rq->ave_seqcnt);
 	rq->ave_nr_running = do_avg_nr_running(rq);
 	rq->nr_last_stamp = rq->clock_task;
 	rq->nr_running--;
