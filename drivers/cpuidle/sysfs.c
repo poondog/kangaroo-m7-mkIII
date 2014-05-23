e457502001c77c59173d16938253bad7845bf5ec
poondog <markj338@gmail.com>
2014-05-23 11:56:35 +1000

    Revert "cpuidle: use drv instead of cpuidle_driver in show_current_driver()"
    This reverts commit afab3d939c45feb078bff92368bd58e98bea1815.

--- drivers/cpuidle/sysfs.c
+++ drivers/cpuidle/sysfs.c
@@ -52,12 +52,12 @@ static ssize_t show_current_driver(struct device *dev,
 				   char *buf)
 {
 	ssize_t ret;
-	struct cpuidle_driver *drv;
+	struct cpuidle_driver *cpuidle_driver;
 
 	spin_lock(&cpuidle_driver_lock);
-	drv = cpuidle_get_driver();
-	if (drv)
-		ret = sprintf(buf, "%s\n", drv->name);
+	cpuidle_driver = cpuidle_get_driver();
+	if (cpuidle_driver)
+		ret = sprintf(buf, "%s\n", cpuidle_driver->name);
 	else
 		ret = sprintf(buf, "none\n");
 	spin_unlock(&cpuidle_driver_lock);
