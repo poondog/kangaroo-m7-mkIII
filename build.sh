ad7b59349f4380d281e4c7886cbe7b25b4ece5cd
poondog <markj338@gmail.com>
2014-05-26 22:35:02 +1000

    BUILD: v072
    Signed-off-by: poondog <markj338@gmail.com>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 # Kernel Version
 BASE_VER=".KANGAROO"
-VER=".M7.v071"
+VER=".M7.v072"
 VER=$BASE_VER$VER
 
 # AK Variables
--- buildwls.sh
+++ buildwls.sh
@@ -4,7 +4,7 @@
 
 # Kernel Version
 BASE_VER=".KANGAROO"
-VER=".M7wls.v071"
+VER=".M7wls.v072"
 VER=$BASE_VER$VER
 
 # AK Variables
