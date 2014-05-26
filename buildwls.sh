7363321c22cc34822c1fa2a5ad0d0414d92f5f18
poondog <markj338@gmail.com>
2014-05-26 20:52:52 +1000

    BUILD: clone build script for sprint
    Signed-off-by: poondog <markj338@gmail.com>

---
+++ buildwls.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Kangaroo Kernel build script thanks to CyanogenMod, anarkia1976
+
+# Kernel Version
+BASE_VER=".KANGAROO"
+VER=".M7.v069"
+VER=$BASE_VER$VER
+
+# AK Variables
+export LOCALVERSION=""`echo $VER`
+
+# toolchain
+export CROSS_COMPILE=${HOME}/Toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.05/bin/arm-cortex_a15-linux-gnueabihf-
+
+# make .config
+make m7_defconfig
+env KCONFIG_NOTIMESTAMP=true \
+make ARCH=arm export SUBARCH=arm CROSS_COMPILE=arm-eabi-
+
+# build the kernel
+
+make -j4
