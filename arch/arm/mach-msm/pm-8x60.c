1350f296179a81c02259c3dfc2e0cc7c08bff54e
poondog <markj338@gmail.com>
2014-06-02 18:14:24 +1000

    Revert "msm: pm-8x60: add suspend_again handler"
    This reverts commit a9d85af659a89bf5cde4d99e5ff01933c3b834ef.

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -22,7 +22,6 @@
 #include <linux/cpu.h>
 #include <linux/pm.h>
 #include <linux/pm_qos.h>
-#include <linux/quickwakeup.h>
 #include <linux/smp.h>
 #include <linux/suspend.h>
 #include <linux/tick.h>
@@ -1364,9 +1363,6 @@ enter_exit:
 static struct platform_suspend_ops msm_pm_ops = {
 	.enter = msm_pm_enter,
 	.valid = suspend_valid_only_mem,
-#ifdef CONFIG_QUICK_WAKEUP
-	.suspend_again = quickwakeup_suspend_again,
-#endif
 };
 
 
