ed7de5b4eda0a77830c06bbd2c84042028b3a963
poondog <markj338@gmail.com>
2014-08-08 00:37:53 +1000

    SMARTMAX: utilize CPUFREQ RELATION_C
    Signed-off-by: poondog <markj338@gmail.com>

--- drivers/cpufreq/cpufreq_smartmax.c
+++ drivers/cpufreq/cpufreq_smartmax.c
@@ -429,11 +429,11 @@ inline static void target_freq(struct cpufreq_policy *policy,
 			// to ramp up to *at least* current + ramp_up_step.
 			if (new_freq > old_freq && prefered_relation == CPUFREQ_RELATION_H
 					&& !cpufreq_frequency_table_target(policy, table, new_freq,
-							CPUFREQ_RELATION_L, &index))
+							CPUFREQ_RELATION_C, &index))
 				target = table[index].frequency;
 			// simlarly for ramping down:
 			else if (new_freq < old_freq
-					&& prefered_relation == CPUFREQ_RELATION_L
+					&& prefered_relation == CPUFREQ_RELATION_C
 					&& !cpufreq_frequency_table_target(policy, table, new_freq,
 							CPUFREQ_RELATION_H, &index))
 				target = table[index].frequency;
@@ -462,7 +462,7 @@ static void cpufreq_smartmax_freq_change(struct smartmax_info_s *this_smartmax) 
 	unsigned int old_freq;
 	int ramp_dir;
 	struct cpufreq_policy *policy;
-	unsigned int relation = CPUFREQ_RELATION_L;
+	unsigned int relation = CPUFREQ_RELATION_C;
 
 	ramp_dir = this_smartmax->ramp_dir;
 	old_freq = this_smartmax->old_freq;
@@ -1379,7 +1379,7 @@ static int cpufreq_governor_smartmax(struct cpufreq_policy *new_policy,
 		else if (this_smartmax->cur_policy->cur < new_policy->min) {
 			dprintk(SMARTMAX_DEBUG_JUMPS,"CPUFREQ_GOV_LIMITS jumping to new min freq: %d\n",new_policy->min);
 			__cpufreq_driver_target(this_smartmax->cur_policy,
-					new_policy->min, CPUFREQ_RELATION_L);
+					new_policy->min, CPUFREQ_RELATION_C);
 		}
 		mutex_unlock(&this_smartmax->timer_mutex);
 		break;
