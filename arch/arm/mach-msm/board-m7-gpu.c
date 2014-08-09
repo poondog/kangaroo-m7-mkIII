b2ec8bdfed51fbd8f9b757e961421c0cdca737f6
poondog <markj338@gmail.com>
2014-08-10 01:14:40 +1000

    GPU: initial GPU overclock -> Allow the GPU to hit a high of 487MHz but keep the 400MHz step    Now the scaling freqs are 27, 128, 200, 320, 400, 487MHz. Hopefully this helps improve gaming performance
    Referenced this commit https://github.com/brymaster5000/m7-443/commit/5543c2ad9ff3539611f51a6ab6db4f566b1ac996
    butchered by yours truely
    
    Signed-off-by: poondog <markj338@gmail.com>

--- arch/arm/mach-msm/board-m7-gpu.c
+++ arch/arm/mach-msm/board-m7-gpu.c
@@ -233,13 +233,12 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
                         .bus_freq = 4,
                         .io_fraction = 0,
                 },
-#else
+#endif
 		{
                         .gpu_freq = 400000000,
                         .bus_freq = 4,
                         .io_fraction = 0,
                 },
-#endif
                 {
                         .gpu_freq = 320000000,
                         .bus_freq = 3,
@@ -261,7 +260,11 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
                 },
         },
         .init_level = 1,
-        .num_levels = 5,
+#ifdef CONFIG_GPU_OVERCLOCK
+        .num_levels = 6,
+#else
+	.num_levels = 5,
+#endif
         .set_grp_async = NULL,
         .idle_timeout = HZ/10,
         .nap_allowed = true,
