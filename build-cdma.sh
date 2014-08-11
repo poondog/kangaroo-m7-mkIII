ad00bca5d3b9408b0448da906d42fadaec8620c9
poondog <markj338@gmail.com>
2014-08-11 15:50:11 +1000

    BUILD: v133
    Signed-off-by: poondog <markj338@gmail.com>

--- build-cdma.sh
+++ build-cdma.sh
@@ -4,7 +4,7 @@
 
 # Kernel Version
 BASE_VER=".KANGAROO"
-VER=".M7.mkIII.wlv.wls.v132"
+VER=".M7.mkIII.wlv.wls.v133"
 VER=$BASE_VER$VER
 
 # AK Variables
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 # Kernel Version
 BASE_VER=".KANGAROO"
-VER=".M7.mkIII.v132"
+VER=".M7.mkIII.v133"
 VER=$BASE_VER$VER
 
 # AK Variables
